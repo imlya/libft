20a0b89114132f912d81e7b39c6993b61a0e0ffc
imatek <imene.atek@gmail.com>
2024-05-20 22:28:46 +0200

    po

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,1 +1,6 @@
+#include "libft.h"
 
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	
+}
--- ft_memset.c
+++ ft_memset.c
@@ -1,1 +1,13 @@
+#include "libft.h"
 
+void	*ft_memset(void *s, int c, size_t n)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] && i < n)
+	{
+		s[i++] = c;
+	}
+	return (s);
+}
