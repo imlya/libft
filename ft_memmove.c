9d27c8bdeeeea83ff3818d71d3fe0ea6e583f380
imatek <imene.atek@gmail.com>
2024-05-21 14:59:55 +0200

    k

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: imatek <imatek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 11:05:48 by imatek            #+#    #+#             */
-/*   Updated: 2024/05/21 13:28:56 by imatek           ###   ########.fr       */
+/*   Updated: 2024/05/21 14:59:35 by imatek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,30 @@ void    *ft_memmove(void *dest, const void *src, size_t n)
 	s_dest = (char *)dest;
 	if (!src)
 		return (NULL);
-	i = 0;
-	while (i < n)
+	if (dest < src)
 	{
-		s_dest[i] = s_src[i];
-		i++;
+		i = 0;
+		while (s_src[i] && i < n)
+		{
+			s_dest[i] = s_src[i];
+			i++;
+		}
+		return (dest);
 	}
-	return (dest);
+	else
+	{
+		i = n - 1;
+		while (i >= 0)
+		{
+			s_dest[i] = s_src[i];
+			i--;
+		}
+		return (dest);
+	}
+}
+
+int	main()
+{
+	__builtin_printf("%s", (char *)ft_memmove("hello", "youu", 1));
+	return (0);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: imatek <imatek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 13:26:44 by imatek            #+#    #+#             */
-/*   Updated: 2024/05/21 13:28:12 by imatek           ###   ########.fr       */
+/*   Updated: 2024/05/21 14:44:32 by imatek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,19 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
+	void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, char *src, size_t size);
 size_t	ft_strlcat(char *dst, char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
+	char	*ft_strchr(const char *s, int c);
+	char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+	void	*ft_memchr(const void *s, int c, size_t n);
+	int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(char *haystack, char *needle, size_t len);
 int		ft_atoi(const char *nptr);
-void	*ft_calloc(size_t nmemb, size_t size);
+	void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 
 #endif
